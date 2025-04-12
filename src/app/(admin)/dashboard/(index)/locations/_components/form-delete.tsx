371b0994c370b6b711c284d71c9d9156638831ac
Maan-py <mluqmaan22@gmail.com>
2025-04-12 10:57:53 +0700

    feat(location): add location management pages

---
+++ src/app/(admin)/dashboard/(index)/locations/_components/form-delete.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ActionResult } from "@/types";
+import { Trash } from "lucide-react";
+import React from "react";
+import { useFormState, useFormStatus } from "react-dom";
+import { deleteLocation } from "../lib/actions";
+
+const initialState: ActionResult = {
+  error: "",
+};
+
+interface FormDeleteProps {
+  id: number;
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" variant={"destructive"} size={"sm"} disabled={pending}>
+      <Trash className="w-4 h-4" />
+      {pending ? "Loading..." : "Delete"}
+    </Button>
+  );
+}
+
+export default function FormDelete({ id }: FormDeleteProps) {
+  const deleteLocationWithId = (_: unknown, formData: FormData) => deleteLocation(_, formData, id);
+  const [state, formAction] = useFormState(deleteLocationWithId, initialState);
+  return (
+    <form action={formAction}>
+      <SubmitButton />
+    </form>
+  );
+}
---
+++ src/app/(admin)/dashboard/(index)/locations/columns.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Location } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { Edit } from "lucide-react";
+import Link from "next/link";
+import FormDelete from "./_components/form-delete";
+
+export const columns: ColumnDef<Location>[] = [
+  {
+    accessorKey: "name",
+    header: "Location name",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const location = row.original;
+
+      return (
+        <div className="space-x-5 flex">
+          <Button size={"sm"} asChild>
+            <Link href={`/dashboard/locations/edit/${location.id}`}>
+              <Edit className="w-4 h-4 mr-2" />
+              Edit
+            </Link>
+          </Button>
+          <FormDelete id={location.id} />
+        </div>
+      );
+    },
+  },
+];
