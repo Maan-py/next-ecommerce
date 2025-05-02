ff8ef75f5189c80894f12e0b00d512d7e7c007a9
Maan-py <mluqmaan22@gmail.com>
2025-05-02 16:11:49 +0700

    chore(deps): replace react-flickity with keen-slider

--- src/app/(customer)/(index)/detail-product/[id]/_components/carousel-images.tsx
+++ src/app/(customer)/(index)/detail-product/[id]/_components/carousel-images.tsx
@@ -1,29 +1,32 @@
 "use client";
 import React from "react";
-import dynamic from "next/dynamic";
-
-const Flickity = dynamic(() => import("react-flickity-component"), {
-  ssr: false,
-});
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
 interface CarouselImagesProp {
   images: string[];
 }
 
 export default function CarouselImages({ images }: CarouselImagesProp) {
+  const [sliderRef] = useKeenSlider<HTMLDivElement>({
+    loop: false,
+    slides: {
+      perView: "auto",
+      spacing: 20,
+    },
+  });
+
   return (
-    <div id="details-images" className="main-carousel overflow-x-hidden mt-[30px]">
-      <Flickity
-        options={{ cellAlign: "left", contain: true, pageDots: false, prevNextButtons: false }} // takes flickity options {}
-      >
+    <div id="details-images" className="overflow-x-hidden mt-[30px]">
+      <div ref={sliderRef} className="keen-slider">
         {images.map((item, i) => (
-          <div key={item + i} className="image-card pr-5 first-of-type:pl-[calc((100vw-1130px-20px)/2)]">
+          <div key={item + i} className="keen-slider__slide image-card pr-5 first-of-type:pl-[calc((100vw-1130px-20px)/2)]">
             <div className="bg-white w-[470px] h-[350px] p-10 flex shrink-0 border border-[#E5E5E5] justify-center items-center rounded-[30px] overflow-hidden">
               <img src={item} className="w-full h-full object-contain" alt="thumbnail" />
             </div>
           </div>
         ))}
-      </Flickity>
+      </div>
     </div>
   );
 }
