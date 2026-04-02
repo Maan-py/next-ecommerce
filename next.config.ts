9d5c3c7a3c6858100e5fbb0a4e6858938099b957
Maan <mluqmaan22@gmail.com>
2026-04-02 20:50:04 +0700

    fix: fix hostname for image

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "nmeyxdaparxdglwzhxkm.supabase.co",
+        hostname: "smhikxpddzswzdxpmwsc.supabase.co",
       },
     ],
   },
